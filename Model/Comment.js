c51381ee049514da08ff1c346d88de0e36648001
ahm7 <elpopahmed462@gmail.com>
2020-04-27 01:42:26 +0200

    likes and dislikes backEnd Done

--- Model/Comment.js
+++ Model/Comment.js
@@ -6,20 +6,58 @@ class Comment extends Entity{
         super(sequelize);
     }  
 
-    async getCommentbyPost(post){
+    async getCommentbyPost(post_id){
         try{
             const { QueryTypes } = require('sequelize');
-            var response = await this.sequelize.query("Select * from Comment as c inner join Entity as e on e.id = c.id where c.post_id="+ post,{ type: QueryTypes.SELECT});
+            var response = await this.sequelize.query("Select c.id  ,content,firstName,lastName,role ,u.id as userid from Comment  c inner join Post  p on p.id = c.post_id inner join Entity  e on e.id = c.id  inner join User u on u.id = e.user_id where p.id="+ post_id,{ type: QueryTypes.SELECT});
+            var comments = []
+            console.log('llop');
+            console.log(response.length);
 
+            for (let i=0;i<response.length;i++){
+                console.log(response[i]);
+            var response2 = await this.sequelize.query("Select L.user_id , c.id , L.up from Comment as c  inner join Likes as L on L.Entity_id = c.id where c.id = "+ response[i].id,{ type: QueryTypes.SELECT});
+            //comments.push(response2);
+           
+             response[i].comment_likes = response2;
+        }
+        
             return response;
 
         }catch(error){
 
-            Console.log('Get Comment By Post Failed');
+            console.log('Get Comment By id Failed');
+            console.log(error)
             return null;
 
         }
     }
+
+    async getCommentInDetails(comment_id){
+        try{
+            const { QueryTypes } = require('sequelize');
+            var response = await this.sequelize.query("Select c.id ,content,firstName,lastName,role ,u.id from Comment as c inner join Entity as e on e.id = c.id  inner join User as u on u.id = e.user_id where c.id="+ comment_id,{ type: QueryTypes.SELECT});
+            comments = []
+            console.log('llop');
+            console.log(response.length);
+
+            for (let i=0;i<response.length;i++){
+                console.log(response[i]);
+            var response2 = await this.sequelize.query("Select L.user_id , c.id from Comment as c  inner join Likes as L on L.id = c.id where c.id = "+ response[i].id,{ type: QueryTypes.SELECT});
+            comments.append(response2);
+        
+        }
+        console.log(comments);
+            return response;
+
+        }catch(error){
+
+            Console.log('Get Comment By id Failed');
+            return null;
+
+        }
+    }
+
 
     async createComment(commment){
         try{
--- Model/Entity.js
+++ Model/Entity.js
@@ -15,12 +15,29 @@ async Likes(entity_id,user_id,upvote,history) {
     var response = null;
     if (history){
 
-      response = await this.sequelize.query("Delete from Likes where user_id ="+user_id,{ type: QueryTypes.DELETE});
+      response = await this.sequelize.query("Delete from Likes where user_id ="+user_id+" and Entity_id ="+ entity_id,{ type: QueryTypes.DELETE});
     
     }
       response = await this.sequelize.query("Insert into Likes(user_id,Entity_id,up) values("+ user_id+","+entity_id+","+upvote+ ")",{ type: QueryTypes.INSERT});
+      
+
+    return true;
+    }catch(error){
+      console.log(error)
+      console.log('Likes Failed');
+      return null;
+    }
+   
+}
+async unLike(entity_id,user_id) {
+     
+  try{
+    const { QueryTypes } = require('sequelize');
+    var response = null;
+
+      response = await this.sequelize.query("Delete from Likes where user_id = "+user_id+" and Entity_id = "+ entity_id,{ type: QueryTypes.DELETE});
     
-    return response;
+    return true;
     }catch(error){
       
       console.log('Likes Failed');
@@ -30,12 +47,31 @@ async Likes(entity_id,user_id,upvote,history) {
 
 
 }
+async checkIfUserLikedEntity(Entity_id,user_id){
 
+  try{
+
+    const { QueryTypes } = require('sequelize');
+    var response = await this.sequelize.query("select * from Likes where Entity_id ="+ Entity_id+" and user_id ="+user_id,{type: QueryTypes.SELECT});
+    console.log(response)
+    console.log('test')
+   
+  return response;
+ 
+   }catch(error){
+     console.log(error)
+     Console.log('check if user like failed');
+    return null;
+   }
+   
+
+}
 async getLikesNumber(Entity_id,up){
   try{
    const { QueryTypes } = require('sequelize');
    var response = await 
    this.sequelize.query("select count(*) from Likes where Entity_id ="+ Entity_id+" And up =" +up,{type: QueryTypes.SELECT});
+   
    return response[0]['count(*)'];
 
   }catch(error){
